c6cd5719a1288048a7441b91efe07bc3665b8c4c
cygnusxi <racingoblivious@gmail.com>
2014-12-02 13:58:59 -0500

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The curecoin developers
+// Copyright (c) 2013-2014 The curecoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <string>
