184e7a9f8d5c3dc3a7009525e1f72f2f1bfc4844
cygnusxi <racingoblivious@gmail.com>
2015-06-11 02:50:01 -0400

    commit id

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("curecoin");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "99999"
+#    define GIT_COMMIT_ID "c744c10"
 #    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
