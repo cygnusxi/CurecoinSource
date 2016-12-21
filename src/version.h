7b123c35b9b76014d263baf702864ac4a8c94061
cygnusxi <racingoblivious@gmail.com>
2016-12-21 04:50:40 -0500

    Update Version 1.3.4

--- src/version.h
+++ src/version.h
@@ -25,7 +25,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60006;
+static const int PROTOCOL_VERSION = 60007;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
@@ -47,6 +47,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 #define DISPLAY_VERSION_MAJOR       0
 #define DISPLAY_VERSION_MINOR       1
 #define DISPLAY_VERSION_REVISION    3
-#define DISPLAY_VERSION_BUILD       3
+#define DISPLAY_VERSION_BUILD       4
 
 #endif
