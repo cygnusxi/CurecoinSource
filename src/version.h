e6d6042922863a0dd064bebb0c4900cb7ee3833c
cygnus <metalhead55544@yahoo.com>
2017-09-27 19:09:13 -0400

    1.9.4.1 Version bump

--- src/version.h
+++ src/version.h
@@ -46,7 +46,7 @@ static const int MEMPOOL_GD_VERSION = 60002;
 
 #define DISPLAY_VERSION_MAJOR       1
 #define DISPLAY_VERSION_MINOR       9
-#define DISPLAY_VERSION_REVISION    3
+#define DISPLAY_VERSION_REVISION    4
 #define DISPLAY_VERSION_BUILD       1
 
 #endif
