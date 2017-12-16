e4a5c3ac0cb28cd9e947a12e44621188049cbb71
EverGreenCoin <EverGreenCoinDev@gmail.com>
2017-12-15 23:07:36 -0500

    add staking=0 support

--- src/init.cpp
+++ src/init.cpp
@@ -243,6 +243,7 @@ std::string HelpMessage()
         "  -listen                " + _("Accept connections from outside (default: 1 if no -proxy or -connect)") + "\n" +
         "  -bind=<addr>           " + _("Bind to given address. Use [host]:port notation for IPv6") + "\n" +
         "  -dnsseed               " + _("Find peers using DNS lookup (default: 0)") + "\n" +
+        "  -staking               " + _("Stake your coins to support the network and gain rewards (default: 1)") + "\n" +
         "  -nosynccheckpoints     " + _("Disable sync checkpoints (default: 0)") + "\n" +
         "  -banscore=<n>          " + _("Threshold for disconnecting misbehaving peers (default: 100)") + "\n" +
         "  -bantime=<n>           " + _("Number of seconds to keep misbehaving peers from reconnecting (default: 86400)") + "\n" +
