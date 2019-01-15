b68d80f41105767d2a01f3caecf01557166f596b
cygnusxi <racingoblivious@gmail.com>
2019-01-15 17:18:05 -0600

    version 2.0.0.2

--- src/version.h
+++ src/version.h
@@ -47,6 +47,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 #define DISPLAY_VERSION_MAJOR       2
 #define DISPLAY_VERSION_MINOR       0
 #define DISPLAY_VERSION_REVISION    0
-#define DISPLAY_VERSION_BUILD       1
+#define DISPLAY_VERSION_BUILD       2
 
 #endif
