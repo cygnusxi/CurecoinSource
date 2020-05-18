80f815130a682bfdf0338483fcc8b86a67cc7ecb
cygnusxi <racingoblivious@gmail.com>
2020-05-17 23:18:38 -0500

    version 2.1.0.1 update

--- src/version.h
+++ src/version.h
@@ -45,8 +45,8 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 #define DISPLAY_VERSION_MAJOR       2
-#define DISPLAY_VERSION_MINOR       0
+#define DISPLAY_VERSION_MINOR       1
 #define DISPLAY_VERSION_REVISION    0
-#define DISPLAY_VERSION_BUILD       2
+#define DISPLAY_VERSION_BUILD       1
 
 #endif
